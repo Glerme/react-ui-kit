efeb6a52a5dc9e6ea0e397a27b275a2cfa8ec8a5
Gui <guiggff@gmail.com>
2022-03-25 15:19:12 -0300

    :wrench: Refatorado Avatar

--- src/components/Avatar/Avatar.stories.tsx
+++ src/components/Avatar/Avatar.stories.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 
-import { Story, ComponentMeta } from "@storybook/react";
+import { Story, ComponentMeta, Meta } from "@storybook/react";
+
+import { action } from "@storybook/addon-actions";
 
 import Avatar from "./Avatar";
 import { AvatarProps } from "./Avatar.types";
@@ -8,22 +10,52 @@ import { AvatarProps } from "./Avatar.types";
 export default {
   title: "Avatar/Avatar",
   component: Avatar,
-  argTypes: {
-    label: {
-      defaultValue: "User",
-    },
-    hasDropdown: {
-      defaultValue: true,
-    },
-    picture: { defaultValue: "https://via.placeholder.com/150" },
-    notificationNumber: {
-      defaultValue: 10,
-    },
-    href: { defaultValue: "#" },
-    onClick: { action: "clicked" },
-  },
-} as ComponentMeta<typeof Avatar>;
+} as Meta<typeof Avatar>;
 
 const Template: Story<AvatarProps> = (args) => <Avatar {...args} />;
 
-export const Default = Template.bind({});
+export const Complete = Template.bind({});
+
+Complete.args = {
+  label: "User",
+  hasDropdown: true,
+  picture: "https://via.placeholder.com/150",
+  notificationNumber: 10,
+  href: "",
+  onClick: action("clicked"),
+};
+
+export const Link = Template.bind({});
+
+Link.args = {
+  label: "User",
+  hasDropdown: true,
+  picture: "https://via.placeholder.com/150",
+  notificationNumber: 10,
+  href: "#",
+};
+
+export const WithoutDropdown = Template.bind({});
+
+WithoutDropdown.args = {
+  label: "User",
+  picture: "https://via.placeholder.com/150",
+  notificationNumber: 10,
+};
+
+export const WithoutName = Template.bind({});
+
+WithoutName.args = {
+  picture: "https://via.placeholder.com/150",
+  notificationNumber: 10,
+};
+
+export const WithoutNotification = Template.bind({});
+
+WithoutNotification.args = {
+  picture: "https://via.placeholder.com/150",
+};
+
+export const WithoutAvatar = Template.bind({});
+
+WithoutAvatar.args = {};
--- src/components/ErrorDetails/ErrorDetails.stories.tsx
+++ src/components/ErrorDetails/ErrorDetails.stories.tsx
@@ -6,16 +6,21 @@ import { ErrorDetailsProps } from "./ErrorDetails.types";
 export default {
   title: "Error/Error Details",
   component: ErrorDetails,
-  argTypes: {
-    error: {
-      defaultValue: {
-        isErrored: true,
-        seeDetails: false,
-      },
-    },
-  },
 } as ComponentMeta<typeof ErrorDetails>;
 
 const Template: Story<ErrorDetailsProps> = (args) => <ErrorDetails {...args} />;
 
 export const Default = Template.bind({});
+
+Default.args = {
+  error: {
+    defaultValue: {
+      isErrored: true,
+      seeDetails: false,
+      error: {
+        name: "Error",
+        message: "Error message",
+      },
+    },
+  },
+};
